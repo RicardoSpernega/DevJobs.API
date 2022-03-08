6126dcabe79974d83571c00ed4fe9b8836e5a34b
ricardo rollo <ricardo_srd@hotmail.com>
2022-03-08 15:06:00 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/JobVacanciesController.cs
@@ -0,0 +1,39 @@
+﻿namespace DevJobs.API.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/job-vacancies")]
+    [ApiController]
+    public class JobVacanciesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        //GET api/job-vacancies/2
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+
+        //POST api/job-vacancies
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        //PUT api/job-vacancies/4
+        [HttpPut("{id}")]
+        public IActionResult Put()
+        {
+            return NoContent();
+        }
+
+
+    }
+}
